73910674364f21fcdd057c99a1cfa8e076f6e630
MadupuriRajesh <madupurirajesh@gmail.com>
2024-09-15 15:24:44 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
 
   output: 'export',
-  assetPrefix: './',
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/portfolio-website/' : '',
   basePath: '/portfolio-website',
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
