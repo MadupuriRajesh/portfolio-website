265f928dbb6970a9726da28e05a2f0cc0b270ff3
MadupuriRajesh <madupurirajesh@gmail.com>
2024-11-25 11:26:06 +0530

    code updated

--- next.config.js
+++ next.config.js
@@ -1,27 +1,39 @@
-const path = require('path')
+// const path = require('path')
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
+// module.exports = {
+//   sassOptions: {
+//     includePaths: [path.join(__dirname, 'styles')],
+//   },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '**',
+//       },
+//       {
+//         protocol: 'https',
+//         hostname: 'media.dev.to',
+//         pathname: '**',
+//       },
+//     ],
+//     unoptimized: true,
+//   },
+//   basePath: isProd ? '/portfolio-website' : '', 
+//   assetPrefix: isProd ? '/portfolio-website/' : '', 
+//   output: 'export',
+// }
+
+const nextConfig = {
+  reactStrictMode: true,
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'res.cloudinary.com',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'media.dev.to',
-        pathname: '**',
-      },
-    ],
-    unoptimized: true,
+    loader: "akamai",
+    path: "",
   },
-  basePath: isProd ? '/portfolio-website' : '', 
-  assetPrefix: isProd ? '/portfolio-website/' : '', 
-  output: 'export',
-}
+  basePath: "/portfolio-website",
+  assetPrefix: "/portfolio-website",
+};
+
+module.exports = nextConfig;
